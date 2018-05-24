57ea3548cecad4e440340605f26d4cf429ad5a3d
AALFRED <AALFRED@AALFRED-PC>
2018-05-23 22:03:47 -0400

    vista editacliente

---
+++ BeLifeV2/editarCliente.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using MahApps.Metro.Behaviours;
+using MahApps.Metro.Actions;
+
+namespace BeLifeV2
+{
+    /// <summary>
+    /// Lógica de interacción para editarCliente.xaml
+    /// </summary>
+    public partial class editarCliente : MetroWindow
+    {
+        public editarCliente()
+        {
+            InitializeComponent();
+        }
+    }
+}
